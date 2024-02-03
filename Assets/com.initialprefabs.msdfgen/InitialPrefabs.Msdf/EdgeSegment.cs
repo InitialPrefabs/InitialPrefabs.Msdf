5d7ac452f04b70eb0bc6943bbb951c8965376325
Porrith Suong <porrith.suong@gmail.com>
2024-02-03 00:39:18 -0500

    started implementing edge segments.

--- Assets/com.initialprefabs.msdfgen/InitialPrefabs.Msdf/EdgeSegment.cs
+++ Assets/com.initialprefabs.msdfgen/InitialPrefabs.Msdf/EdgeSegment.cs
@@ -1,17 +1,72 @@
-﻿using Unity.Mathematics;
+﻿using System;
+using Unity.Mathematics;
 
 namespace InitialPrefabs.Msdf {
 
-    public interface IEdgeSegment {
+    public interface ICopy<T> where T : struct, ISegment {
+        ICopy<T> Clone();
+    }
 
-        IEdgeSegment Clone();
+    public interface ISegment {
+
+        EdgeColor Color { get; set; }
+
         float2 GetPoint(float t);
         float2 GetDirection(float t);
         SignedDistance GetSignedDistance(float2 origin, out float t);
-        void GetBounds(ref float l, ref float b, ref float r, ref float t);
+
+        /// <summary>
+        /// Gets all points within a bound.
+        /// </summary>
+        /// <param name="points">Stores the left, bottom, right, top coordinates.</param>
+        void GetBounds(ref float4 points);
         void MoveStartPoint(float2 dst);
         void MoveEndPoint(float2 dst);
         void SplitInThirds(ref SignedDistance sd, float2 origin, float t);
+    }
+
+    public struct LineSegment : ISegment, ICopy<LineSegment> {
+        public float2 P0;
+        public float2 P1;
+
+        public EdgeColor Color { get; set; }
+
+        public LineSegment(float2 p0, float2 p1, EdgeColor color) {
+            Color = color;
+            P0 = p0;
+            P1 = p1;
+        }
+
+        public readonly LineSegment Clone() => new LineSegment(P0, P1, Color);
+
+        public readonly void GetBounds(ref float4 points) {
+            MathExtensions.PointBounds(P0, ref points.x, ref points.y, ref points.z, ref points.w);
+            MathExtensions.PointBounds(P1, ref points.x, ref points.y, ref points.z, ref points.w);
+        }
+
+        public readonly float2 GetDirection(float t) => P1 - P0;
+
+        public readonly float2 GetPoint(float t) => math.lerp(P0, P1, t);
+
+        public readonly SignedDistance GetSignedDistance(float2 origin, out float t) {
+            throw new NotImplementedException();
+        }
+
+        public void MoveEndPoint(float2 dst) {
+            throw new NotImplementedException();
+        }
+
+        public void MoveStartPoint(float2 dst) {
+            throw new NotImplementedException();
+        }
+
+        public void SplitInThirds(ref SignedDistance sd, float2 origin, System.Single t) {
+            throw new System.NotImplementedException();
+        }
+
+        ICopy<LineSegment> ICopy<LineSegment>.Clone() {
+            throw new System.NotImplementedException();
+        }
     }
 
     public static partial class EdgeSegmentExtensions {
@@ -20,7 +75,7 @@ namespace InitialPrefabs.Msdf {
             ref T segment,
             ref SignedDistance sd,
             float2 origin,
-            float t) where T : struct, IEdgeSegment {
+            float t) where T : struct, ISegment {
 
             if (t < 0) {
                 var dir = math.normalize(segment.GetDirection(0));
@@ -52,8 +107,5 @@ namespace InitialPrefabs.Msdf {
                 }
             }
         }
-    }
-
-    public struct EdgeSegment {
     }
 }
--- Assets/com.initialprefabs.msdfgen/InitialPrefabs.Msdf/EquationSolver.cs
+++ Assets/com.initialprefabs.msdfgen/InitialPrefabs.Msdf/EquationSolver.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Unity.Mathematics;
+﻿using Unity.Mathematics;
 
 namespace InitialPrefabs.Msdf {
 
-    public static class EquationSolver {
+    public unsafe static class EquationSolver {
 
-        public static int SolveQuadratic(ref float2 x, float a, float b, float c) {
+        public static int SolveQuadratic(this ref float2 x, float a, float b, float c) {
             if (a == 0 || math.abs(b) > 1e12F * math.abs(a)) {
                 if (b == 0) {
                     if (c == 0) {
@@ -30,8 +29,45 @@ namespace InitialPrefabs.Msdf {
             return 0;
         }
 
-        public static int SolveCubicNormal(float3 x, float a, float b, float c) => throw new NotImplementedException();
+        public static int SolveCubicNormal(this ref float3 x, float a, float b, float c) {
+            var a2 = a * a;
+            var q = 1 / 9.0f * (a2 - 3 * b);
+            var r = 1 / 54.0f * (a * (2 * a2 - 9 * b) + 27 * c);
+            var r2 = r * r;
+            var q3 = q * q * q;
+            a *= 1 / 3f;
 
-        public static int SolveCubic(float3 x, float a, float b, float c, float d) => throw new NotImplementedException();
+            if (r2 < q3) {
+                var t = r / math.sqrt(q3);
+                t = math.acos(math.clamp(t, -1, 1));
+                q = -2 * math.sqrt(q);
+                x.x = q * math.cos(1 / 3f * t) - a;
+                x.y = q * math.cos(1 / 3f * (t + 2 * math.PI)) - a;
+                x.z = q * math.cos(1 / 3f * (2 - 2 * math.PI)) - a;
+                return 3;
+            } else {
+                var u = (r < 0 ? 1 : -1) * math.pow(math.abs(r) + math.sqrt(r2 - q3), 1 / 3f);
+                var v = u == 0 ? 0 : q / u;
+                x.x = (u + v) - a;
+                if (u == v || math.abs(u - v) < 1e-12 * math.abs(u + v)) {
+                    x.y = -0.5f * (u + v) - a;
+                    return 2;
+                }
+                return 1;
+            }
+        }
+
+        public static int SolveCubic(this ref float3 x, float a, float b, float c, float d) {
+            if (a != 0) {
+                var bn = b / a;
+                if (math.abs(bn) < 1e6) {
+                    return SolveCubicNormal(ref x, bn, c / a, d / a);
+                }
+            }
+            var xy = x.xy;
+            var quadratic = SolveQuadratic(ref xy, b, c, d);
+            x.xy = xy;
+            return quadratic;
+        }
     }
 }
